2f0b9da295d2af99c3d9ad5a48b62945fbba0865
Ajay Vishnu E <ajayvishnu121@gmail.com>
2024-11-12 00:06:07 +0530

    minor changes

--- src/containers/ConnectSection/ConnectSection.jsx
+++ src/containers/ConnectSection/ConnectSection.jsx
@@ -1,37 +1,74 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './connectsection.css'
 import { ConnectSectionData } from '../../constants/constants'
 
 function ConnectSection() {
-  return (
-    <div className='connectwithme-container'>
-        <p className='connectwithme-h'>CONNECT WITH ME</p>
-        <p className='connectwithme-c1'>Are you looking for the <label style={{color:"#3F8AE2", fontWeight:"500"}}>perfect design</label>?</p>
-        <p className='connectwithme-c2'>Then you’re in the right place. Get the best designs you’re looking for. Just <label style={{color:"#3F8AE2", fontWeight:"500"}}>reach out </label>and let me know!</p>
-        <p className='image-square-frame connectwithme-email'><span></span>{ConnectSectionData.email}</p>
-        <p className='connectwithme-phone'><label className='cwm-phone-label-1'>{ConnectSectionData.phone_conutry_code}&nbsp;</label><label className='cwm-phone-label-2'>{ConnectSectionData.phone}</label></p>
-        <div className='connectwithme-logos-container'>
-            <a href={ConnectSectionData.linkedin.link}>
-                <ConnectSectionData.linkedin.logo/>
-            </a>
-            <a href={ConnectSectionData.instagram.link}>
-                <ConnectSectionData.instagram.logo/>
-            </a>
-            <a href={ConnectSectionData.medium.link}>
-                <ConnectSectionData.medium.logo/>
-            </a>
-            <a href={ConnectSectionData.faceboook.link}>
-                <ConnectSectionData.faceboook.logo/>
-            </a>
-            <a href={ConnectSectionData.youtube.link}>
-                <ConnectSectionData.youtube.logo/>
-            </a>
-            <a href={ConnectSectionData.x.link}>
-                <ConnectSectionData.x.logo/>
-            </a>
+    const label1Ref = useRef(null);
+    const label2Ref = useRef(null);
+
+    useEffect(() => {
+        const label1 = label1Ref.current;
+        const label2 = label2Ref.current;
+
+        const handleMouseEnterLabel1 = () => {
+            label1.style.color = '#3F8AE2';
+            label2.style.color = '#1A1A1A';
+        };
+        const handleMouseEnterLabel2 = () => {
+            label1.style.color = '#3F8AE2';
+            label2.style.color = '#1A1A1A';
+        };
+
+        const handleMouseLeaveLabel1 = () => {
+            label1.style.color = 'transparent';
+            label2.style.color = 'transparent';
+        };
+        const handleMouseLeaveLabel2 = () => {
+            label1.style.color = 'transparent';
+            label2.style.color = 'transparent';
+        };
+
+        label1.addEventListener('mouseenter', handleMouseEnterLabel1);
+        label1.addEventListener('mouseleave', handleMouseLeaveLabel1);
+        label2.addEventListener('mouseenter', handleMouseEnterLabel2);
+        label2.addEventListener('mouseleave', handleMouseLeaveLabel2);
+
+        return () => {
+            label1.removeEventListener('mouseenter', handleMouseEnterLabel1);
+            label1.removeEventListener('mouseleave', handleMouseLeaveLabel1);
+            label2.removeEventListener('mouseenter', handleMouseEnterLabel2);
+            label2.removeEventListener('mouseleave', handleMouseLeaveLabel2);
+        };
+    }, []);
+    return (
+        <div className='connectwithme-container'>
+            <p className='connectwithme-h'>CONNECT WITH ME</p>
+            <p className='connectwithme-c1'>Are you looking for the <label style={{color:"#3F8AE2", fontWeight:"500"}}>perfect design</label>?</p>
+            <p className='connectwithme-c2'>Then you’re in the right place. Get the best designs you’re looking for. Just <label style={{color:"#3F8AE2", fontWeight:"500"}}>reach out </label>and let me know!</p>
+            <p className='image-square-frame connectwithme-email'><span></span>{ConnectSectionData.email}</p>
+            <p className='connectwithme-phone'><label ref={label1Ref} className='cwm-phone-label-1'>{ConnectSectionData.phone_conutry_code}&nbsp;</label><label ref={label2Ref} className='cwm-phone-label-2'>{ConnectSectionData.phone}</label></p>
+            <div className='connectwithme-logos-container'>
+                <a href={ConnectSectionData.linkedin.link}>
+                    <ConnectSectionData.linkedin.logo/>
+                </a>
+                <a href={ConnectSectionData.instagram.link}>
+                    <ConnectSectionData.instagram.logo/>
+                </a>
+                <a href={ConnectSectionData.medium.link}>
+                    <ConnectSectionData.medium.logo/>
+                </a>
+                <a href={ConnectSectionData.faceboook.link}>
+                    <ConnectSectionData.faceboook.logo/>
+                </a>
+                <a href={ConnectSectionData.youtube.link}>
+                    <ConnectSectionData.youtube.logo/>
+                </a>
+                <a href={ConnectSectionData.x.link}>
+                    <ConnectSectionData.x.logo/>
+                </a>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ConnectSection
